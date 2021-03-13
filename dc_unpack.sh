2313eae7ee8a680397f68140988ad4370ac82618
wouterVE <vaneenoo.wouter@gmail.com>
2021-03-13 18:47:54 +0100

    added makedir option & added code to convert nrg files

--- dc_unpack.sh
+++ dc_unpack.sh
@@ -2,14 +2,16 @@
 mode=$1
 source=$2
 destination=$3
+mk_dir=$4
 
 #check if variables are entered
 if [ "$destination" = "" ]
 then
 echo "no mode,source or destination directory entered"
-echo "usage: ./dc_unpack.sh <MODE> <SOURCE dir> <DESTINATION dir>"
-echo "mode d: use directories as input"
-echo "mode f: use files as input"
+echo "usage: ./dc_unpack.sh <MODE> <SOURCE dir> <DESTINATION dir> <OPTION: (m)>"
+echo "mode=d: use directories as input"
+echo "mode=f: use files as input"
+echo "m: create separate directories for each game - only use when archive does not contain directory"
 exit 1
 fi
 
@@ -32,13 +34,13 @@ touch "$destination"/"extracted.txt"
 fi
 
 
-#check mode file - dir
+#check mode file/dir
 if [ "$mode" = d ]
 then
-#list all directories in list
+#create list of all directories
 ls "$source" -p | grep / | cut -f1 -d'/' > "$destination"/list.txt
 else
-#list all files in list
+#create list of all files
 ls "$source" -p | grep -v / | cut -f1 -d'/' > "$destination"/list.txt
 fi
 
@@ -53,9 +55,39 @@ else
 if [ "$mode" = "f" ] 
 then
 #file mode
-find "$source"/"$game" -name "*.rar" -exec unrar x -y {} "$destination" \;
-echo "$game" >> "$destination"/"extracted.txt"
+
+ #check if mk_dir option is entered
+if [ "$mk_dir" = "m" ]
+ then
+ # make separate dirs for each game
+
+ # extract extension
+ # see https://stackoverflow.com/a/965072
+ filename=$(basename -- "$game")
+ extension="${filename##*.}"
+ gamedir="${filename%.*}"
+ mkdir "$destination"/"$gamedir"
+ unrar x -y "$source"/"$game" "$destination"/"$gamedir"
+ echo "$gamedir" >> "$destination"/"extracted.txt"
+
+#13/03/2021:Temporarily disable -> check for which files this code applies to
+#find "$destination" -name "*part01.rar" -print0 | while read -r -d $'\0' partrar
+#do
+#   unrardir=$(dirname "$partrar")
+#   #unrar archive
+#   unrar x -y "$partrar" "$unrardir/"
+#   #delete partially archives
+#   find "$unrardir/" -name "*.part*.rar" -exec rm -i -f {} \;
+#done
+
+ else
+ #do NOT make separate dirs
+ find "$source"/"$game" -name "*.rar" -exec unrar x -y {} "$destination" \;
+ echo "$game" >> "$destination"/"extracted.txt"
+ fi
+
 fi
+
 if [ "$mode" = "d" ]
 then
 #dir mode
@@ -71,30 +103,14 @@ fi
 done < "$destination"/list.txt
 
 
-#check for ecm in extracted file & unecm them
-if [ "$mode" = "f" ]
-then
-#file mode
-echo
-fi
-
-if [ "$mode" = "d" ]
-then
-#dir mode
-while IFS= read -r game ; do
-find "$destination"/"$game" -name "*.ecm" -exec unecm {} "$destination"/"$game"/"$game".cdi \;
-find "$destination"/"$game" -name "*.ecm" -exec rm {} \;
-done < "$destination"/list.txt
-fi
-
-
 #https://stackoverflow.com/questions/5119946/find-exec-with-multiple-commands
 
 #find & extract partially archives
 #https://stackoverflow.com/a/9612560
 
 #RAR
-find "$destination" -name "*part01.rar" -print0 | while read -d $'\0' partrar
+#13/03/2021: possibly not necessary as *.rar would also extract *part01.rar
+find "$destination" -name "*part01.rar" -print0 | while read -r -d $'\0' partrar
 do
    unrardir=$(dirname "$partrar")
    #unrar archive
@@ -104,7 +120,7 @@ do
 done
 
 #001
-find "$destination" -name "*.001" -print0 | while read -d $'\0' partrar
+find "$destination" -name "*.001" -print0 | while read -r -d $'\0' partrar
 do
    unrardir=$(dirname "$partrar")
    #unrar archive
@@ -114,16 +130,45 @@ do
 done
 
 #rar
-find "$destination" -name "*.rar" -print0 | while read -d $'\0' partrar
+find "$destination" -name "*.rar" -print0 | while read -r -d $'\0' partrar
 do
    unrardir=$(dirname "$partrar")
    #unrar archive
    unrar x -y "$partrar" "$unrardir/"
    #delete partially archives
    find "$unrardir/" -name "*.r??" -exec rm -i -f {} \;
-   #delete .sfv files
+   #delete remaining .sfv files
    find "$destination" -name "*.sfv" -exec rm -i -f {} \;
+   find "$destination" -name "*.SFV" -exec rm -i -f {} \;
 done
+
+#check for ecm & nrg in extracted file & unecm them
+if [ "$mode" = "f" ]
+then
+#file mode
+while IFS= read -r game ; do
+#ecm
+find "$destination"/"$game" -name "*.ecm" -exec unecm {} "$destination"/"$game"/"$game".cdi \;
+find "$destination"/"$game" -name "*.ecm" -exec rm {} \;
+#nrg
+find "$destination"/"$game" -name "*.nrg" -exec nrg2iso {} "$destination"/"$game"/"$game".iso \;
+find "$destination"/"$game" -name "*.nrg" -exec rm {} \;
+done <"$destination"/extracted.txt
+echo
+fi
+
+if [ "$mode" = "d" ]
+then
+#dir mode
+while IFS= read -r game ; do
+#ecm
+find "$destination"/"$game" -name "*.ecm" -exec unecm {} "$destination"/"$game"/"$game".cdi \;
+find "$destination"/"$game" -name "*.ecm" -exec rm {} \;
+#nrg
+find "$destination"/"$game" -name "*.nrg" -exec nrg2iso {} "$destination"/"$game"/"$game".iso \;
+find "$destination"/"$game" -name "*.nrg" -exec rm {} \;
+done < "$destination"/list.txt
+fi
 
 
 
